afd8f7524f970b6600b7e690bf2c48c9ed6ff242
RFarl <113222875@umail.ucc.ie>
2015-01-05 12:24:36 +0000

    Chapter 2: Program Structure (looping a triangle)

---
+++ loopingATriangle.js
@@ -0,0 +1,13 @@
+ tri = "";
+ count = 0;
+while (count <= 7){
+	count++;
+	console.log(tri)
+	 tri = tri + "#";
+}
+
+// or: 
+tri2 = "";
+for (i = 0; i < 7; i ++) {
+console.log(tri2 = tri2+"#")
+}
